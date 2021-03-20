9278242e5d23726ddfc1369c8fa6abadd3aff971
h-lixuan <39815091+h-lixuan@users.noreply.github.com>
2021-03-21 01:00:40 +0800

    linking buttons to respective pages + start of registration page

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+    else:
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
