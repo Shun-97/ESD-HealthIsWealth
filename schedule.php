9a6c0c513be7e24c2e431e294c560cf7f0dd07d4
Shun <lishunhui97@gmail.com>
2021-04-14 11:44:34 +0800

    done

--- schedule.php
+++ schedule.php
@@ -17,7 +17,6 @@
   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
   <script src="js/ScriptLoader.js"></script>
   <script src="js/navbar.js"></script>
-  <link rel="stylesheet" href="css/main.css">
   
   <!-- full calendar -->
   <link href='External_Files/FullCalendar/main.css' rel='stylesheet' />
