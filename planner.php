52d6406a23e87d59143f8c7e5786ada43aa9ca3f
h-lixuan <39815091+h-lixuan@users.noreply.github.com>
2021-04-14 14:23:35 +0800

    uncomment redirect to landing_plan

--- planner.php
+++ planner.php
@@ -257,7 +257,7 @@
                 }).then((response)=> response.json())
                 .then((data)=>{
                     console.log(data)
-                    // window.location.replace("./landing_plan.php")
+                    window.location.replace("./landing_plan.php")
                 })
             },
             deleteFood: function(id){
